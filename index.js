c2be994546704bb0c064cb1bdeff107ec5c77d5f
Omarabdullah99 <omarabdullah917303@gmail.com>
2022-11-08 19:48:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const express=require('express')
+const jwt=require('jsonwebtoken')
+const app=express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const cors=require('cors')
+require('dotenv').config()
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uxycogs.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+
+
+async function run(){
+    try{
+        const serviceCollection=client.db('cooking').collection('services')
+
+        //all data read
+        app.get('/services', async(req,res)=>{
+            const query={};
+            const cursor=serviceCollection.find(query)
+            const services=await cursor.toArray()
+            res.send(services)
+        })
+
+        app.get('/serviceshome', async(req,res)=>{
+            const query={};
+            const cursor=serviceCollection.find(query)
+            const services=await cursor.limit(3).toArray()
+            res.send(services)
+        })
+        
+        //single service
+        app.get('/services/:id', async(req,res)=>{
+            const id=req.params.id;
+            const query={_id:ObjectId(id)}
+            const service= await serviceCollection.findOne(query)
+            res.send(service)
+        })
+       
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+const port =process.env.port || 4001
+app.get('/',(req,res)=>{
+    res.send(' assignment 11  server is running ')
+})
+
+
+
+app.listen(port,()=>{
+    console.log( ` nodemon server is running ${port}`)
+})
+
+//password:GzxJ8GZAqYQlJcmU username:dbuser
